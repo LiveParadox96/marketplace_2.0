137eef9b0d23e3d6a0be07b119e53e881eec91d9
Maksim El'kin <LiveParadox@vk.com>
2022-12-04 06:45:05 +0300

    minor changes

--- src/pages/card-detail/index.jsx
+++ src/pages/card-detail/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { APP_CONSTANTS } from "../../config/contants";
 import NewCard from "../main/Content/NewCard/NewCard";
 import Button from "../main/Content/components/Button/Button";
@@ -8,11 +8,12 @@ import './index.css'
 
 
 
+
 const CardDetailPage = (props) => {
   console.log(props)
   const { productId } = useParams();
   const [currentCardInformation, setCurrentCardInformation] = useState(null);
-
+  const navigate = useNavigate();
   useEffect(
     function getInformationByProductId() {
       const CURRENT_PRODUCT_URL = `${APP_CONSTANTS.API.PRODUCTS_URL}/${productId}`;
@@ -29,7 +30,7 @@ const CardDetailPage = (props) => {
     return "Загрузка...";
   }
   return  (<>
-            <a className="prev" to="/" href="http://localhost:3000">На главную</a>
+          <h3 onClick={() => navigate(-1)}>На главную</h3>
             <div className="flex_NewCard">
               <NewCard item={currentCardInformation}/>
             </div>
